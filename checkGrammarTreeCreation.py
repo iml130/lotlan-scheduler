82a4111049101b1daebb4d4ecf4c731acdf6aa8e
Lux, Dominik <dominik.lux@iml.fraunhofer.de>
2019-03-06 10:03:49 +0100

    Added ErrorListener

--- checkGrammarTreeCreation.py
+++ checkGrammarTreeCreation.py
@@ -1,14 +1,31 @@
 from antlr4 import *
+from antlr4.error.ErrorListener import ErrorListener
 from TaskLexer import TaskLexer
 from TaskParserListener import TaskParserListener
 from TaskParser import TaskParser
 from CreateTreeTaskParserVisitor import CreateTreeTaskParserVisitor
 import sys
 
+class ThrowErrorListener(ErrorListener):
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        raise e
+
+    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
+        raise Exception("Task-Language could not be parsed")
+
+    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
+        raise Exception("Task-Language could not be parsed")
+
+    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
+        raise Exception("Task-Language could not be parsed")
+
+
 def main():
     lexer = TaskLexer(InputStream(open("examples.txt").read()))
+    lexer._listeners.append(ThrowErrorListener())
     stream = CommonTokenStream(lexer)
     parser = TaskParser(stream)
+
     tree = parser.program() 
     visitor = CreateTreeTaskParserVisitor()
     # printer = TaskParserListener()
