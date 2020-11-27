d12c37eeca3898b789cfcc56f8097e0934c905f8
Maximilian Hörstrup <maximilian.hoerstrup@iml.fraunhofer.de>
2020-11-27 16:21:06 +0100

    Add TransportOrder state

--- lotlan_schedular/api/transportorder.py
+++ lotlan_schedular/api/transportorder.py
@@ -1,5 +1,7 @@
 """ Contains TransportOrder class """
 
+from enum import IntEnum
+
 # local sources
 from lotlan_schedular.api.transportorder_step import TransportOrderStep
 
@@ -8,6 +10,13 @@ class TransportOrder:
         Represents a TransportOrder in lotlan code
         as well as in scheduling
     """
+
+    class TransportOrderState(IntEnum):
+        WAIT_FOR_TRIGGERED_BY = 0
+        TRANSPORT_ORDER_STARTED = 1
+        WAIT_FOR_FINISHED_BY = 2
+        FINISHED = 3
+
     def __init__(self):
         self.uuid = ""
         self.to_step_from = TransportOrderStep()
@@ -15,6 +24,7 @@ class TransportOrder:
         self.from_parameters = []
         self.to_parameters = []
         self.task_name = ""
+        self.state = None
 
     def __str__(self):
         return (("\n UUID: {}\n To_Step_From: \t\t {} \n To_Step_To: \t\t {} \n\t")
